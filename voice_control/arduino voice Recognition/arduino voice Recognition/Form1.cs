26ab70701b9dda23f582cabaaf7647941a8fb2ee
bciavare <32144980+bciavare@users.noreply.github.com>
2017-11-15 11:03:09 -0500

    Add files via upload

---
+++ voice_control/arduino voice Recognition/arduino voice Recognition/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO.Ports;
+
+
+namespace arduino_voice_Recognition
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            button2.Enabled = true;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices command = new Choices();
+            command.Add(new string[] { "light on", "light off" });
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(command);
+            Grammar grammar = new Grammar(gBuilder);
+
+            recEngine.LoadGrammarAsync(grammar);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += RecEngine_SpeechRecognized;
+        }
+
+        void RecEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            synthesizer.SpeakAsync("What would you like to do?");
+            switch (e.Result.Text)
+            {
+                case "light on":             
+                    SerialPort port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One); //Change COM5 to your com.
+                    port.Open();
+                    port.Write("1"); //the data that will be sent to the arduino, change if it is needed.
+                    port.Close();
+                    richTextBox1.Text += "\nlight on";
+                    break;
+                case "light off":
+                    SerialPort port1 = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One); //Change COM5 to your com.
+                    port1.Open();
+                    port1.Write("0"); //the data that will be sent to the arduino, change if it is needed.
+                    port1.Close();
+                    richTextBox1.Text += "\nlight off";
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            button2.Enabled = false;
+        }
+    }
+}
---
+++ voice_control/voice_control.ino
@@ -0,0 +1,26 @@
+int led = 13; // Pin 13
+void setup()
+{
+pinMode(led, OUTPUT); // Set pin 13 as digital out
+// Start up serial connection
+Serial.begin(9600); // baud rate
+Serial.flush();
+}
+void loop()
+{
+String input = "";
+// Read any serial input
+while (Serial.available() > 0)
+{
+input += (char) Serial.read(); // Read in one char at a time
+delay(5); // Delay for 5 ms so the next char has time to be received
+}
+if (input == "1") //if the arduino got "1" from the serial communication
+{
+digitalWrite(led, HIGH); // on
+}
+else if (input == "0") //if the arduino got "0" from the serial communication
+{
+digitalWrite(led, LOW); // off
+}
+}
